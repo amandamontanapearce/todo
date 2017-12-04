540d195d682266331eba51b6260048bf4697036d
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2017-12-04 13:05:41 -0700

    refactor list to a computed property

--- resources/assets/js/components/ToDo.vue
+++ resources/assets/js/components/ToDo.vue
@@ -6,16 +6,10 @@
         <h1 class="text-center p-3">You don't have a list to-do started today, just add a task to create your daily to-do list!</h1>
       </div>
       <ul class="list-group mt-5">
-        <li class="list-group-item mt-1" v-if="!value.isCompleted" v-for="(value, key, index) item in list">
-          <span :class="{ 'highPriority' : value.isHighPriority }">{{value.task}}</span>
-          <span @click="completedTask(value)"><i class="fa fa-check-square-o"></i></span>
-          <span class="float-right" @click="removeTask(index)">remove task</span>
-        </li>
-      </ul>
-      <ul class="list-group mt-5" v-for="item in list">
-        <li class="list-group-item" v-if="item.isCompleted">
-          <strike :class="{ 'highPriority' : item.isHighPriority }">{{item.task}}</strike>
-          <span class="float-right" @click="removeTask(index)">remove task</span>
+        <li class="list-group-item mt-1" v-for="(value, key, index) in displayList">
+          <span :class="{ 'highPriority' : value.isHighPriority, 'completed' : value.isCompleted }">{{value.task}}</span>
+          <span @click="completedTask(value)"><i class="fa fa-lg fa-check-square-o"></i></span>
+          <span class="float-right" @click="removeTask(index)"><small>remove task</small></span>
         </li>
       </ul>
       <div class="card card-body mt-5 mb-5">
@@ -30,13 +24,13 @@
           <div class="form-group">
             <div class="form-check form-check-inline">
               <label class="form-check-label">
-            <input class="form-check-input" type="radio" value="true" v-model="isHighPriority">high
-          </label>
+                <input class="form-check-input" type="radio" value="true" v-model="isHighPriority">high
+              </label>
             </div>
             <div class="form-check form-check-inline">
               <label class="form-check-label">
-            <input class="form-check-input" type="radio" value="false" v-model="isHighPriority">normal
-          </label>
+                <input class="form-check-input" type="radio" value="false" v-model="isHighPriority">normal
+              </label>
             </div>
           </div>
         </form>
@@ -49,6 +43,10 @@
 <style>
 .highPriority {
   color: red;
+}
+
+.completed {
+  text-decoration: line-through;
 }
 
 #backgroundImageContainer {
@@ -68,7 +66,12 @@ export default {
     return {
       isHighPriority: false,
       task: '',
-      list: [],
+      list: [
+        {task: 'brush teeth', isCompleted: false, isHighPriority: true},
+        {task: 'wash face', isCompleted: false, isHighPriority: false},
+        {task: 'pet puppy', isCompleted: true, isHighPriority: true},
+        {task: 'get dressed', isCompleted: false, isHighPriority: false}
+      ],
       backgroundImageSrc: '',
     }
   },
@@ -88,12 +91,22 @@ export default {
       this.list.splice(index, 1);
     },
   },
+  computed: {
+  displayList() {
+    if(this.list) {
+      let order = _.orderBy(this.list, ['isCompleted', 'isHighPriority'], ['aced','desc']);
+      console.log(order);
+      return order;
+      } else {
+      return [];
+      }
+    },
+
+  },
   mounted() {
     console.log('mounted');
-    if (this.list.length < 1) {
       let backgroundImage = axios.create({
         baseURL: 'https://source.unsplash.com/',
-
       });
       backgroundImage.get('daily?nature').then(function(response) {
         let backgroundImageProp = "url('" + response.request.responseURL + "')";
@@ -101,7 +114,6 @@ export default {
         let element = document.getElementById("backgroundImageContainer");
         element.setAttribute("style", "background-image:" + backgroundImageProp);
       });
-    }
   },
 }
 </script>
