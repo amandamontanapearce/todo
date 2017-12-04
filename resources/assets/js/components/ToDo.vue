f8d36f6be296067e78a6f7746c65253774b8bbaf
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2017-12-04 00:12:13 -0700

    add background image and adjust styling. Bring in axios to ToDo component and get image from unsplash.

--- resources/assets/js/components/ToDo.vue
+++ resources/assets/js/components/ToDo.vue
@@ -1,24 +1,23 @@
 <template>
-<div class="container">
-  <h1>To-Do List, get to it!
-
-    <a href="#addToTask">
-    <span class="float-right"><i class="fa fa-plus"></i></span>
-    </a>
-    </h1>
-  <ul class="list-group" v-for="(value, key, index) item in list">
+<div id="backgroundImageContainer" class="container mt-3">
+<div class="row">
+<div class="col">
+<div v-if="this.list.length < 1" class="card card-body opaqueBackground mt-5">
+<h1 class="text-center p-3">You don't have a list to-do started today, just add a task to create your daily to-do list!</h1>
+</div>
+  <ul class="list-group mt-5" v-for="(value, key, index) item in list">
     <li class="list-group-item" v-if="!value.isCompleted">
       <span :class="{ 'highPriority' : value.isHighPriority }">{{value.task}}</span>
       <span @click="completedTask(value)"><i class="fa fa-check-square-o"></i></span>
       <span class="float-right" @click="removeTask(index)">remove task</span>
     </li>
   </ul>
-  <ul class="list-group" v-for="item in list">
+  <ul class="list-group mt-5" v-for="item in list">
     <li class="list-group-item" v-if="item.isCompleted">
       <strike :class="{ 'highPriority' : item.isHighPriority }">{{item.task}}</strike>
     </li>
   </ul>
-  <div class="card card-body">
+  <div class="card card-body mt-5 mb-5">
     <h2 id="addToTask">Add to the list...</h2>
     <form class="mt-3">
       <div class="form-group">
@@ -42,21 +41,35 @@
     </form>
   </div>
 </div>
+</div>
+</div>
 </template>
 
 <style>
 .highPriority {
   color: red;
 }
+
+#backgroundImageContainer {
+  background-color: #cccccc;
+}
+
+.opaqueBackground {
+  background-color: hsla(100, 100%, 100%, .5);
+}
+
 </style>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       isHighPriority: false,
       task: '',
       list: [],
+      backgroundImageSrc: '',
     }
   },
   methods: {
@@ -75,6 +88,20 @@ export default {
       this.list.splice(index, 1);
     },
   },
-  mounted() {},
+  mounted() {
+    console.log('mounted');
+    if(this.list.length < 1) {
+      let backgroundImage = axios.create({
+      baseURL: 'https://source.unsplash.com/',
+
+      });
+      backgroundImage.get('daily?nature').then( function(response) {
+      let backgroundImageProp = "url('" + response.request.responseURL + "')";
+      console.log(backgroundImageProp);
+      let element = document.getElementById("backgroundImageContainer");
+      element.setAttribute("style", "background-image:" + backgroundImageProp);
+      });
+    }
+  },
 }
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Daily Todo List</title>
 
         <!-- Icons -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -18,17 +18,25 @@
 
     </head>
     <body>
-        <div id="app">
-          <div class="container-fluid">
-
-          </div>
-          <to-do></to-do>
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <span class="d-inline-block align-top"><i class="fa fa-2x fa-list-alt"></i></span>
+          To-Do List, get to it!
+        </a>
+            <a href="#addToTask">
+            <span class="float-right">
+              <i class="fa fa-2x fa-plus"></i>
+            </span>
+            </a>
+      </nav>
+        <div id="app" class="container-fluid">
+            <to-do></to-do>
         </div>
 
-        <div class="flex-center position-ref full-height">
+        <div class="text-center">
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="title m-3">
+                    <small>@copyright Amanda Montana Pearce 2017</small>
                 </div>
             </div>
         </div>
