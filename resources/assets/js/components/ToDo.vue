8a26bf2cbfe3a1bb1285a69395d992d48065eba0
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2017-12-06 18:39:53 -0700

    cosmetics add icon and adjust opaqueBackground

--- resources/assets/js/components/ToDo.vue
+++ resources/assets/js/components/ToDo.vue
@@ -61,7 +61,7 @@
         </form>
       </div>
       <div class="card card-body opaqueBackground mt-4 mb-5">
-        <h3>How are you doing? List statistics.</h3>
+        <h3>How are you doing? List statistics. <i class="fa fa-line-chart"></i></h3>
         <p>You've complete {{percentComplete}}% of your tasks! <span v-if="percentComplete > 90"> Great Job!</span></p>
         <p>{{percentHighPriority}}% of your tasks are high priority.</p>
       </div>
